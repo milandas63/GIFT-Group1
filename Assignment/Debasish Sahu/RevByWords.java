05257b37fdf3e1cc040cae2ed0efcb9a9433ab33
DeV <138612303+D3BAS1SH@users.noreply.github.com>
2023-07-11 13:25:47 -0700

    Reversing a string in different ways.
    1) TotalReverse.java Holds the program to reverse a string which would be given and like From "Gandhi Institute For Technology" to "ygolonhceT roF etutitsnI ihdanG"
    2) RevByWords.java Holds the program to Reverse the combination of the characters in the words in the String like From "This is My name" to "sihT si yM eman".
    3) RevOnlyWords.java Holds the program to Reverse the Combinations of the words in the string like From "My Name is Dante" to "Dante is Name My".

---
+++ Assignment/Debasish Sahu/RevByWords.java
@@ -0,0 +1,35 @@
+import java.io.*;
+
+public class RevByWords {
+    public static void main(String[] Args)throws IOException{
+
+        InputStreamReader ISR=new InputStreamReader(System.in);
+        BufferedReader Br= new BufferedReader(ISR);
+
+        System.out.print("Give the Sentence you want to Reverse : ");
+
+        String Sen=Br.readLine()+" ";
+        String Rev=new String("");
+        String Words= new String("");
+        
+        for(int i=0;i<Sen.length();i++){
+            if(Sen.charAt(i)!=' '){
+                Words+=Sen.charAt(i);
+            }
+            else{
+                Rev+=StrRev(Words)+" ";
+                //System.out.println(Rev);
+                Words="";
+            }
+        }
+        System.out.println("Reversed Sentence by Word's character : "+Rev);
+    }
+    public static String StrRev(String Str){
+        String Rev="";
+        for(int i=Str.length()-1;i>=0;i--){
+            Rev+=Str.charAt(i);
+        }
+        //System.out.println("In Reversing Function Returning : "+Rev);
+        return Rev;
+    }
+}
