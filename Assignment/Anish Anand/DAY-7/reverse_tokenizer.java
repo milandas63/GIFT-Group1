faa4174e1503181dc8154be26e34b82829b06915
royanish123 <138609235+royanish123@users.noreply.github.com>
2023-07-13 22:21:51 +0530

    Create reverse_tokenizer.java

---
+++ Assignment/Anish Anand/DAY-7/reverse_tokenizer.java
@@ -0,0 +1,23 @@
+//Write a program to reverse the words in a given String?
+  //            Example: God is Omnipotent and Omniscient
+    //          Result:  Omniscient and Omnipotent is God
+
+
+import java.io.*;
+import java.util.*;
+
+public class ReverseTest{
+    public static void main(String[] args)throws IOException{
+        BufferedReader Br=new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Enter The String : ");
+        //StringBuilder Str= new StringBuilder(Br.readLine());
+        StringBuilder Rev= new StringBuilder("");
+        StringTokenizer Tokens= new StringTokenizer(Br.readLine(), " ");
+        while(Tokens.hasMoreTokens()){
+            Rev.insert(0," "+Tokens.nextToken());
+        }
+        System.out.println("Reversed String : "+ Rev.toString());
+        
+    }
+
+}
