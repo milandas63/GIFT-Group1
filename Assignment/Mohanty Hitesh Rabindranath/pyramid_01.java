c4b736e139dbff92e6aafe0694b4813eb213ae48
Mohanty Hitesh <138584930+Mohanty-Hitesh-20@users.noreply.github.com>
2023-07-13 22:56:47 -0700

    Create pyramid_01.java

---
+++ Assignment/Mohanty Hitesh Rabindranath/pyramid_01.java
@@ -0,0 +1,26 @@
+/*
+	a)	Draw a triangle of asterisks?
+			        *        
+			       ***       
+			      *****      
+			     *******     
+			    *********    
+			   ***********   
+			  *************  
+			 *************** 
+			*****************
+		   *******************
+ */
+
+public class pyramid_01 {
+	public static void main(String[] args) {
+		int spaces = 30;
+		int stars = 1;
+		for(int i=0; i<10; i++) {
+			for(int j=0; j<spaces; j++) System.out.print(" ");
+			for(int j=0; j<stars; j++)  System.out.print("*");
+			System.out.println();
+			spaces--; stars+=2;
+		}
+	}
+}
