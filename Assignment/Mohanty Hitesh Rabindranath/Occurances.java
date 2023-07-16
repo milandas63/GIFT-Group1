30ef2bed08f6df19ea662a32d28229234a737990
Mohanty Hitesh <138584930+Mohanty-Hitesh-20@users.noreply.github.com>
2023-07-16 20:25:43 +0530

    Create Occurances.java

---
+++ Assignment/Mohanty Hitesh Rabindranath/Occurances.java
@@ -0,0 +1,22 @@
+public class Occurances {
+        public static void main(String[] args) {
+            String words = "A place where someone thinks about you is a place you can call home";
+            int count[] = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
+            char c;
+            for(int i=0; i<words.length(); i++) {
+                c = words.charAt(i);
+                if(c>=65 && c<=90) {
+                    count[c-65]++;
+                } else if(c>=97 && c<=122) {
+                    count[c-97]++;
+                }
+            }
+    
+            for(int i=0; i<count.length; i++) {
+                if(count[i]>0) {
+                    System.out.println( (char)(i+65) + " = " + count[i] );
+                }
+            }
+        }
+    }
+
