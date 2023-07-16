d6a970874a0856574ec13d024e757dc6a765b066
Mohanty Hitesh <138584930+Mohanty-Hitesh-20@users.noreply.github.com>
2023-07-16 19:20:29 +0530

    Update multipletable.java

--- Assignment/Mohanty Hitesh Rabindranath/multipletable.java
+++ Assignment/Mohanty Hitesh Rabindranath/multipletable.java
@@ -34,7 +34,7 @@
 		System.out.println();
 		for(int h=2; h<=10; h+=5) {
 			for(int i=1; i<=10; i++) {
-				for(int j=h; j<=(h+3); j++) {
+				for(int j=h; j<=(h+4); j++) {
 					System.out.print(pad(j,2)+" x "+pad(i,2)+" = "+pad((j*i),3)+"  ");
 					if(j>=12) break;
 				}
