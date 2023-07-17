78ec3b8c1694c75cf516302b8ff2b5d9a9a0c12d
royanish123 <138609235+royanish123@users.noreply.github.com>
2023-07-17 11:36:33 +0530

    Update perfect_num.java

--- Assignment/Anish Anand/DAY-8 (14-07-2023)/perfect_num.java
+++ Assignment/Anish Anand/DAY-8 (14-07-2023)/perfect_num.java
@@ -1,1 +1,23 @@
+package com.algo;
 
+public class perfect_num {
+
+	public static void main(String[] args) {
+		int first = 6, last = 10000; 
+		System.out.println("Perfect numbers between " + first + " and " + last + " are :");
+		for (int num = first; num <= last; num++) {
+			if (check_perfect(num)) {
+				System.out.println(num);
+			}
+		}
+	}
+	public static boolean check_perfect(int num) {
+		int s = 0;
+		for (int i = 1; i <= num / 2; i++) {
+			if (num % i == 0) {
+				s += i;
+			}
+		}
+		return s == num;
+	}
+}
