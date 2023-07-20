c08a7ed3fbad9f8e4aa9827b0bbb748144e0bc86
Ankit Kumar <138609224+ankitku3101@users.noreply.github.com>
2023-07-21 01:05:46 +0530

    Add files via upload

---
+++ Assignment/Ankit Kumar/Day 12 20-07-2023/str_box.py
@@ -0,0 +1,12 @@
+text = ['Hello','World','in','a','Frame']
+#txt='\n'.join(text)
+longest=0
+for i in range(0,len(text)):
+    if(len(text[i])>longest):
+        longest=len(text[i])
+
+total=longest+4
+print('*'*(total))
+for i in range(0,len(text)):
+    print('*',text[i],' '*(longest-len(text[i])),end='*\n')
+print('*'*(total))
