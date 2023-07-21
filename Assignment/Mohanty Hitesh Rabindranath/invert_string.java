ee722166363c0e01ea41fc173d550458a14f7c14
Mohanty Hitesh <138584930+Mohanty-Hitesh-20@users.noreply.github.com>
2023-07-21 23:06:50 +0530

    Update and rename invert.java to invert_string.java

--- Assignment/Mohanty Hitesh Rabindranath/invert_string.java
+++ Assignment/Mohanty Hitesh Rabindranath/invert_string.java
@@ -3,7 +3,7 @@ Flip/Invert/Toggle the case of all characters in a given String?
         Original:   Quick Brown Fox Jumps Over The Lazy Dog
         Invert:     qUICK bROWN fOX jUMPS oVER tHE lAZY dOG
  */
-public class invert {
+public class invert_string {
     public static void main(String[]args){
         String str ="Quick Brown Fox Jumps Over The Lazy Dog";
         char[] arr = str.toCharArray();
