43d4e831560a81cdc117db9e976084095c9f8de2
Mohanty Hitesh <138584930+Mohanty-Hitesh-20@users.noreply.github.com>
2023-07-21 21:57:33 +0530

    Create invert.java

---
+++ Assignment/Mohanty Hitesh Rabindranath/invert.java
@@ -0,0 +1,27 @@
+/*
+Flip/Invert/Toggle the case of all characters in a given String?
+        Original:   Quick Brown Fox Jumps Over The Lazy Dog
+        Invert:     qUICK bROWN fOX jUMPS oVER tHE lAZY dOG
+ */
+public class invert {
+    public static void main(String[]args){
+        String str ="Quick Brown Fox Jumps Over The Lazy Dog";
+        char[] arr = str.toCharArray();
+        System.out.println("The Entered String:"+" " + str);
+        for(int i=0;i<arr.length;i++){
+            char c=arr[i];
+            if(Character.isUpperCase(c)){
+                arr[i]=Character.toLowerCase(c);
+            }
+            else if(Character.isLowerCase(c)){
+                arr[i]=Character.toUpperCase(c);
+            }
+        }
+        System.out.print("The inverted String:"+" " );
+        for(int i=0;i<arr.length;i++){
+            System.out.print(arr[i]+"");
+        }
+        }
+
+    }
+
