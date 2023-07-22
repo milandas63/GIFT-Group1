1d1dd1a65c90bb6b1bf1d5fa6f8bef0869b4ba7b
Mohanty Hitesh <138584930+Mohanty-Hitesh-20@users.noreply.github.com>
2023-07-22 18:30:53 +0530

    Update Occurances.java

--- Assignment/Mohanty Hitesh Rabindranath/Occurances.java
+++ Assignment/Mohanty Hitesh Rabindranath/Occurances.java
@@ -1,3 +1,18 @@
+/*
+22: Count and report the number of occurrences of each character in a given string?
+
+    Hello World    Students Allowed       Holocaust
+       D = 1           A - 1                A - 1
+       E - 1           D - 2                C - 1
+       H - 1           E - 2                H - 1
+       L - 3           L - 1                L - 1
+       O - 2           N - 1                O - 2
+       R - 1           O - 1                S - 1
+       W - 1           S - 2                T - 1
+                       T - 2                U - 1
+                       U - 1
+                       W - 1
+*/
 public class Occurances {
         public static void main(String[] args) {
             String words = "A place where someone thinks about you is a place you can call home";
