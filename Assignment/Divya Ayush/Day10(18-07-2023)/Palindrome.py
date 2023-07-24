93cfc004f5b80d9e39358a99de8d233853d03300
DivyaAyush <138608615+DivyaAyush@users.noreply.github.com>
2023-07-24 23:00:38 +0530

    Update Palindrome.py

--- Assignment/Divya Ayush/Day10(18-07-2023)/Palindrome.py
+++ Assignment/Divya Ayush/Day10(18-07-2023)/Palindrome.py
@@ -1,1 +1,18 @@
-
+#          1        
+#         121       
+#        12321      
+#       1234321     
+#      123454321    
+#     12345654321   
+#    1234567654321  
+#   123456787654321 
+#  12345678987654321
+for x in range(0,10):
+	for y in range(1,sp+1):
+		print(" ",end="")
+	for y in range(1,x+1):
+		print(y,end="")
+	for y in range(x-1, 0, -1):
+		print(y,end="")
+	print()
+	sp = sp-1
