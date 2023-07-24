45717705a565845c30b1267b08ebfaba5090b502
DivyaAyush <138608615+DivyaAyush@users.noreply.github.com>
2023-07-24 23:23:17 +0530

    Update File_open.py

--- Assignment/Divya Ayush/Day 13(21-07-2023)/File_open.py
+++ Assignment/Divya Ayush/Day 13(21-07-2023)/File_open.py
@@ -1,1 +1,14 @@
 
+import os
+def PbyS(S):
+    for i in S:
+        print(i,end=' ')
+
+Path=input("PATH : ")
+DList=os.listdir(Path)
+for x in DList:
+    print(x)
+Fname= input("\nFILE NAME : ")
+with open((str(Path+'\\'+Fname)),'r',encoding='cp1252') as F:
+    print(F.read())
+    F.close()
