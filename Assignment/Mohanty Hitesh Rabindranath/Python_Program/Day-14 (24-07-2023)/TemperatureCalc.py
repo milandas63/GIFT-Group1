ae9b964710072bff48966a453070fd80a9c93ed8
Mohanty Hitesh <138584930+Mohanty-Hitesh-20@users.noreply.github.com>
2023-07-24 21:48:32 +0530

    Create TemperatureCalc.py

---
+++ Assignment/Mohanty Hitesh Rabindranath/Python_Program/Day-14 (24-07-2023)/TemperatureCalc.py
@@ -0,0 +1,33 @@
+#  TEMPERATURE CDALCULATOR
+
+def celsius_to_fahrenheit(celsius):
+    return (celsius * 9/5) + 32
+def fahrenheit_to_celsius(fahrenheit):
+    return (fahrenheit - 32) * 5/9
+print("----------Temperature Calculator----------")
+temp = float(input("Enter the temperature: "))
+unit = input("Enter the unit (C, F): ")
+
+if unit.upper() == "C":
+    fahrenheit = celsius_to_fahrenheit(temp)
+    print("Fahrenheit:", fahrenheit)
+elif unit.upper() == "F":
+    celsius = fahrenheit_to_celsius(temp)
+    print("Celsius:", celsius)
+else:
+    print("Invalid unit")
+    exit()
+
+
+"""
+  OUTPUT:
+          ----------Temperature Calculator----------
+          Enter the temperature:258
+          Enter the unit (C, F): c
+          Fahrenheit: 136.4
+  OUTPUT:
+          ----------Temperature Calculator----------
+          Enter the temperature: 125486
+          Enter the unit (C, F): F
+          Celsius: 69696.66666666667
+"""
