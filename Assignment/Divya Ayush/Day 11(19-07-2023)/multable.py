a8e4c1e5acb27d50af34a0ce6ee9a79b8f00e0b4
DivyaAyush <138608615+DivyaAyush@users.noreply.github.com>
2023-07-24 23:06:32 +0530

    Update multable.py

--- Assignment/Divya Ayush/Day 11(19-07-2023)/multable.py
+++ Assignment/Divya Ayush/Day 11(19-07-2023)/multable.py
@@ -1,1 +1,17 @@
 
+def padL(n,l):
+    rValue = str(n)
+    for i in range (len(rValue),l):
+        rValue = ' '+rValue
+    return rValue
+
+start = 2
+end = 11
+for i in range(2, 10+1, 5):
+    for j in range(1, 11):
+        for k in range(i, i+5):
+            if k>end:
+                break
+            print(padL(k,2),"x",padL(j,2),"=",padL((k*j),3)," ",end=' ')
+        print()
+    print()
