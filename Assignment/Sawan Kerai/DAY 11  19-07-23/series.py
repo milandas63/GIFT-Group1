a8c3d8c1af098028e4ce2462baa104582bedca5e
Sawankerai <125033775+Sawankerai@users.noreply.github.com>
2023-07-28 21:50:37 +0530

    Update series.py

--- Assignment/Sawan Kerai/DAY 11  19-07-23/series.py
+++ Assignment/Sawan Kerai/DAY 11  19-07-23/series.py
@@ -1,1 +1,12 @@
+H=int(input("Term length : "))
+count=1
+n=1
+while count<=H:
+    print(int(n),end=', ')
+    count+=1
+    if(count%2==0):
+        n=(2*n)+1
+    else:
+        n=(2*n)-1
 
+print("")  
