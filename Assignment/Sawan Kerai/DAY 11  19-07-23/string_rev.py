a64fe18b646eca805645d9735a80846ae6456747
Sawankerai <125033775+Sawankerai@users.noreply.github.com>
2023-07-28 21:51:23 +0530

    Update string_rev.py

--- Assignment/Sawan Kerai/DAY 11  19-07-23/string_rev.py
+++ Assignment/Sawan Kerai/DAY 11  19-07-23/string_rev.py
@@ -1,1 +1,13 @@
+def reverse(s):
+	str = ""
+	for i in s:
+		str = i + str
+	return str
 
+s = "Gandhi Institute For Technology"
+
+print("The original string is : ", end="")
+print(s)
+
+print("The reversed string is : ", end="")
+print(reverse(s))
