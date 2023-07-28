761f72aaffa05a96e1534fabc555f9ef57cc621f
Sawankerai <125033775+Sawankerai@users.noreply.github.com>
2023-07-28 22:13:39 +0530

    Update py_lambda.py

--- Assignment/Sawan Kerai/DAY-13(21-07-2023)/py_lambda.py
+++ Assignment/Sawan Kerai/DAY-13(21-07-2023)/py_lambda.py
@@ -1,1 +1,14 @@
-
+def SimpleInterestRate(time):
+    if time<=3:
+        interest_amount_func = lambda principal : (principal * 6.0 * time) / 100
+    elif time<=5:
+        interest_amount_func = lambda principal : (principal * 5.5 * time) / 100
+    elif time<=8:
+        interest_amount_func = lambda principal : (principal * 3.5 * time) / 100
+    return interest_amount_func;
+      
+      
+sir = SimpleInterestRate(4)
+print(sir(4000))
+print(sir(3000))
+print(sir(8500))
